9429daa43ce45368fa75d9480f4a83aa9f132286
Mehmet <200201029@kocaeli.edu.tr>
2025-02-28 15:17:49 +0300

    FilmProjesi Excel indirme özelliği eklendi

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
+using OfficeOpenXml; // EPPlus için
+using System.IO;
 
 namespace FilmProjesi.Controllers
 {
@@ -25,6 +27,40 @@ namespace FilmProjesi.Controllers
             });
 
             return RedirectToAction("Index");
+        }
+
+        public IActionResult ExcelIndir()
+        {
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial; // Lisans ayarı
+
+            using (var package = new ExcelPackage())
+            {
+                var sheet = package.Workbook.Worksheets.Add("Filmler");
+
+                // Başlıkları ekleyelim
+                sheet.Cells[1, 1].Value = "Film Adı";
+                sheet.Cells[1, 2].Value = "İngilizce Adı";
+                sheet.Cells[1, 3].Value = "Yapım Yılı";
+                sheet.Cells[1, 4].Value = "Oyuncular";
+                sheet.Cells[1, 5].Value = "IMDB Puanı";
+
+                // Film listesini ekleyelim
+                for (int i = 0; i < Filmler.Count; i++)
+                {
+                    sheet.Cells[i + 2, 1].Value = Filmler[i].FilmAdi;
+                    sheet.Cells[i + 2, 2].Value = Filmler[i].FilmAdiIng;
+                    sheet.Cells[i + 2, 3].Value = Filmler[i].YapimYili;
+                    sheet.Cells[i + 2, 4].Value = Filmler[i].Oyuncular;
+                    sheet.Cells[i + 2, 5].Value = Filmler[i].ImdbPuani;
+                }
+
+                var stream = new MemoryStream();
+                package.SaveAs(stream);
+                stream.Position = 0;
+
+                string excelName = $"FilmListesi_{DateTime.Now:yyyyMMddHHmmss}.xlsx";
+                return File(stream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", excelName);
+            }
         }
     }
 
